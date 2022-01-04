10baad79a27e9652c2e76a31d0fd20e2e742f8f1
Pratik / Guru <12322081+Pratikh@users.noreply.github.com>
2022-01-04 13:13:58 -0600

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,54 +1,86 @@
-import fs from "fs";
-import md5 from "md5";
-import nc from "next-connect";
-import path from "path";
+import nextConnect from 'next-connect';
+import multer from 'multer';
 
-const handler = nc();
-handler.post((req, res) => {
-  try {
-    const { name: fileName, currentChunkIndex, totalChunks } = req.query;
-    const firstChunk = parseInt(currentChunkIndex) === 0;
-    const lastChunk = parseInt(currentChunkIndex) === parseInt(totalChunks) - 1;
-
-    const ext = fileName?.split(".").pop();
-    const data = req.body?.toString().split(",")[1];
-    const buffer = new Buffer(data, "base64");
-    const tmpFilename = "tmp_" + md5(fileName + req.ip) + "." + ext;
-    const uploadPath = path.resolve("./public/");
-    if (firstChunk && fs.existsSync(uploadPath + '/' + tmpFilename)) {
-      fs.unlinkSync(uploadPath + '/' + tmpFilename);
-    }
-    fs.appendFileSync(uploadPath + '/' + tmpFilename, buffer);
-    
-    if (lastChunk) {
-      const finalFilename = md5(Date.now()).substr(0, 6) + "." + ext;
-      fs.renameSync(uploadPath + '/' + tmpFilename, uploadPath + finalFilename);
-      res.status(200).json({
-        success: true,
-        finalFilename,
-        fileName,
-        currentChunkIndex,
-        totalChunks,
-        firstChunk,
-        lastChunk,
-        ext,
-        uploadPath,
-      });
-    } else {
-      res.json(uploadPath + '/' + tmpFilename);
-    }
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error
-    });
-  }
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: './public/uploads',
+    filename: (req, file, cb) => cb(null, file.originalname),
+  }),
 });
 
-// handler.get()
+const apiRoute = nextConnect({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
 
-export default handler;
+apiRoute.use(upload.array('theFiles'));
+
+apiRoute.post((req, res) => {
+  res.status(200).json({ data: 'success' });
+});
+
+export default apiRoute;
+
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
+
+// import fs from "fs";
+// import md5 from "md5";
+// import nc from "next-connect";
+// import path from "path";
+
+// const handler = nc();
+// handler.post((req, res) => {
+//   try {
+//     const { name: fileName, currentChunkIndex, totalChunks } = req.query;
+//     const firstChunk = parseInt(currentChunkIndex) === 0;
+//     const lastChunk = parseInt(currentChunkIndex) === parseInt(totalChunks) - 1;
+
+//     const ext = fileName?.split(".").pop();
+//     const data = req.body?.toString().split(",")[1];
+//     const buffer = new Buffer(data, "base64");
+//     const tmpFilename = "tmp_" + md5(fileName + req.ip) + "." + ext;
+//     const uploadPath = path.resolve("./public/");
+//     if (firstChunk && fs.existsSync(uploadPath + '/' + tmpFilename)) {
+//       fs.unlinkSync(uploadPath + '/' + tmpFilename);
+//     }
+//     fs.appendFileSync(uploadPath + '/' + tmpFilename, buffer);
+//     if (lastChunk) {
+//       const finalFilename = md5(Date.now()).substr(0, 6) + "." + ext;
+//       fs.renameSync(uploadPath + '/' + tmpFilename, uploadPath + finalFilename);
+//       res.status(200).json({
+//         success: true,
+//         finalFilename,
+//         fileName,
+//         currentChunkIndex,
+//         totalChunks,
+//         firstChunk,
+//         lastChunk,
+//         ext,
+//         uploadPath,
+//       });
+//     } else {
+//       res.json(uploadPath + '/' + tmpFilename);
+//     }
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       error
+//     });
+//   }
+// });
+// export default handler;
+
+
 // export default function handler(req, res) {
+  // handler.get()
 //   const { name, currentChunkIndex, totalChunks } = req.query;
 //   console.log("hello", body);
 
